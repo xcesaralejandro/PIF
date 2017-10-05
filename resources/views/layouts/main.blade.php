d27b20955c049edf21ce43d0d4fbd3f9c0ad6d5b
César Alejandro Mora Cid <cesar.mcid@gmail.com>
2017-10-04 23:25:48 -0300

    menu con loguin ok

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html lang="es">
-  <head>
-    <!-- Required meta tags -->
+<html lang="{{ app()->getLocale() }}">
+<head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') - Frust</title>
     <!-- Bootstrap - FontAwesome - Hoja personalizada-->
     <link rel="stylesheet" type="text/css" href="{{asset('recursos/bootstrap/css/bootstrap.min.css')}}">
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -6,34 +6,49 @@
 
   <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
     <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+      {{-- Loguin --}}
+      @guest
+      <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Iniciar sesión</a></li>
+      <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Registrarse</a></li>
+      @else
       <li class="nav-item">
         <div class="dropup nav-item">
           <button class="dropdown-toggle dropup nav-link" type="button" id="about-us" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Usuario apellido
+            {{ Auth::user()->name }}
           </button>
-            <div class="dropdown-menu" aria-labelledby="about-us">
-              <a class="dropdown-item" href="#">Menú 1</a>
-              <a class="dropdown-item" href="#">Menú 2</a>
-              <a class="dropdown-item" href="#">Menú 3</a>
-            </div>
-          </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Buscar persona">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-    </form>
-  </div>
+          <div class="dropdown-menu" aria-labelledby="about-us">
+            <a class="dropdown-item" href="#">Mis datos personales</a>
+            <a class="divider"></a>
+            <a class="dropdown-item" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();">
+            Cerrar sesión
+          </a>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            {{ csrf_field() }}
+          </form>
+        </div>
+      </div>
+    </li>
+    @endguest
+    {{-- Fin menu loguin --}}
+
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+  </ul>
+  <form class="form-inline my-2 my-lg-0">
+    <input class="form-control mr-sm-2" type="text" placeholder="Buscar persona">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+  </form>
+</div>
 </nav>
