3157cb1733fdfbe1414837919d50490e1a40dcfb
gatho0 <alex.esp.f@gmail.com>
2017-10-28 03:35:23 -0300

    Search detalles

---
+++ public/js/sAlimentos.js
@@ -0,0 +1,9 @@
+
+
+$(function(){
+    $("#grupos").chosen({no_results_text: "No se ha podido localizar",
+								placeholder_text_single: "Seleccione Grupo"}); 
+    $("#categorias").chosen({no_results_text: "No se ha podido localizar",
+								placeholder_text_single: "Seleccione categoria"}); 
+
+});
