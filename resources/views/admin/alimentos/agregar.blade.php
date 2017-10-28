f2b9705e1ac5ee4378cf3ab0926b8b3dcf952e4f
César Alejandro Mora Cid <cesar.mcid@gmail.com>
2017-10-28 17:40:35 -0300

    Select alimentos

--- resources/views/admin/alimentos/agregar.blade.php
+++ resources/views/admin/alimentos/agregar.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.mainAdmin') 
+@extends('layouts.mainAdmin')
 @section('title-nav','Administrador')
-@section('title','Administrar alimentos') 
+@section('title','Administrar alimentos')
 
 @section('nav')
     @include('admin.alimentos.nav')
-@endsection 
+@endsection
 
 @section('placeholderBuscar','Buscar alimento') @section('content')
 <div class="text-center mb-4 mt-3">
@@ -20,11 +20,11 @@
             <div class="col-sm-8 offset-sm-2 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4 mt-4 mb-5 pl-5 pr-5">
                 <button type="submit" class="btn btn-primary">
                     <div class="row">
-                        <div class="col-2 text-left"">
-                            <i class="fa fa-plus" aria-hidden="true"></i>   
+                        <div class="col-2 text-left">
+                            <i class="fa fa-plus" aria-hidden="true"></i>
                         </div>
                         <div class="col-10 text-center">
-                            Agregar alimento                      
+                            Agregar alimento
                         </div>
                     </div>
                 </button>
