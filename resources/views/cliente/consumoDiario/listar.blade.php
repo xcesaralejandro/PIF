9746c7d6997f05da46c02fb5f8daa8b9d542c802
César Alejandro Mora Cid <cesar.mcid@gmail.com>
2017-11-03 03:18:30 -0300

    Mejora listado nuevo avance (sug prof) iconos formulas comunes (sug prof)

--- resources/views/cliente/consumoDiario/listar.blade.php
+++ resources/views/cliente/consumoDiario/listar.blade.php
@@ -9,8 +9,11 @@
         <div class="col-sm-4 col-lg-3 col-xl-2">
           <div class="fuckingPadre">
             <a href="{{route('registroDiario.show',$v->id)}}">
-              <div class="cajean3 text-center mb-3 p-4 textoBlanco">
-                  <span class="display-2"><strong>{{$v->cs_fecha}}</strong></span>
+              <div class="cajean3 text-center mb-3 textoBlanco pl-4 pr-4 pb-1">
+                  <div class="fullDiv amuentanx2">
+                      {{date("d",strtotime($v->cs_fecha))}}
+                  </div>
+                  <span class="display-2">{{date("m-Y",strtotime($v->cs_fecha))}}</span>
               </div>
             </a>
           </div>
